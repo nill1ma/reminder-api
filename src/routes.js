8bd71b66e617780183c181c625eb0fbcc679e366
Nil <avanilson.limasantos@gmail.com>
2021-03-26 15:09:56 -0300

    Structure of folders added

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+const MiddleWare = require('./config/MiddleWare')
+const routes = Router()
+
+// routes.post('/users/login', UserController.login)
+// routes.post('/users/create', UserController.create)
+// routes.get('/users', UserController.index).use(MiddleWare)
+// routes.get('/pokemons', Pokemons.index).use(MiddleWare)
+// routes.get('/pokemons/:id', Pokemons.get).use(MiddleWare)
+
+export default routes
